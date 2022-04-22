4956b0688369cbaa033d211e75774baa2289b74e
Ben van Werkhoven <b.vanwerkhoven@esciencecenter.nl>
2022-04-22 11:50:54 +0200

    rewrote dashboard code

--- dashboard.py
+++ dashboard.py
@@ -1,137 +1,109 @@
-#!/usr/bin/env python
 import os
-import sys
-
-from functools import partial
+from kernel_tuner import read_cache # actually kernel_tuner.util read_cache
 
 import panel as pn
 import panel.widgets as pnw
 import pandas as pd
-import bokeh
+from bokeh.models import HoverTool, LinearColorMapper
+from bokeh.plotting import ColumnDataSource, figure
+pn.extension()
 
 
-from bokeh.plotting import ColumnDataSource
-from bokeh.models import HoverTool
-from bokeh.layouts import gridplot
-from bokeh.plotting import figure, output_notebook, show
-from bokeh.models import LinearColorMapper
+class KTdashboard:
+    """ Main object to instantiate to hold everything related to a running dashboard"""
 
-from kernel_tuner.util import read_cache
+    def __init__(self, cache_file, demo=False):
+        self.demo = demo
+        self.cache_file = cache_file
+        # read in the cachefile
+        self.cache_last_modified = os.stat(cache_file).st_mtime
+        cached_data = read_cache(cache_file, open_cache=False)
+        self.kernel_name = cached_data["kernel_name"]
+        self.device_name = cached_data["device_name"]
 
-cache_last_modified = None
+        # get the performance data
+        data = list(cached_data["cache"].values())
+        data = [d for d in data if d["time"] != 1e20]
 
+        # use all data or just the first 1000 in demo mode
+        self.index = len(data)
+        if self.demo:
+            self.index = 1000
 
+        # figure out which keys are interesting
+        single_value_tune_param_keys = [key for key in cached_data["tune_params_keys"] if len(cached_data["tune_params"][key]) == 1]
+        tune_param_keys = [key for key in cached_data["tune_params_keys"] if key not in single_value_tune_param_keys]
+        single_value_keys = [key for key in data[0].keys() if not isinstance(data[0][key],list) and key not in single_value_tune_param_keys]
+        output_keys = [key for key in single_value_keys if key not in tune_param_keys]
+        float_keys = [key for key in output_keys if isinstance(data[0][key], float)]
 
-def create_dashboard(cache_file, demo=False):
+        self.single_value_tune_param_keys = single_value_tune_param_keys
+        self.tune_param_keys = tune_param_keys
+        self.single_value_keys = single_value_keys
+        self.output_keys = output_keys
+        self.float_keys = float_keys
 
-    #read in the cachefile
-    cache_last_modified = os.stat(cache_file).st_mtime
-    cached_data = read_cache(cache_file, open_cache=False)
-    data = list(cached_data["cache"].values())
-    data = [d for d in data if d["time"] != 1e20]
-
-    index = len(data)
-    if demo:
-        index = 1000
-
-    #figure out which keys are interesting
-    single_value_tune_param_keys = [key for key in cached_data["tune_params_keys"] if len(cached_data["tune_params"][key]) == 1]
-    tune_param_keys = [key for key in cached_data["tune_params_keys"] if key not in single_value_tune_param_keys]
-    single_value_keys = [key for key in data[0].keys() if not isinstance(data[0][key],list) and key not in single_value_tune_param_keys]
-    output_keys = [key for key in single_value_keys if key not in tune_param_keys]
-    float_keys = [key for key in output_keys if isinstance(data[0][key], float)]
-
-
-    data_df = pd.DataFrame(data[:index])[single_value_keys]
-    source = ColumnDataSource(data=data_df)
-
-
-    #create selectors
-    yvariable  = pnw.Select(name='Y', value='GFLOP/s',
-                            options=single_value_keys)
-    xvariable  = pnw.Select(name='X', value='index',
-                            options=['index']+single_value_keys)
-    colorvariable  = pnw.Select(name='Color By', value='GFLOP/s',
-                            options=single_value_keys)
-
-
-
-    def update_colors(color_by):
-        color_mapper = LinearColorMapper(palette='Viridis256', low=min(data_df[color_by]), high=max(data_df[color_by]))
-        color = {'field': color_by, 'transform': color_mapper}
-        return color
-
-
-
-    @pn.depends(xvariable, yvariable, colorvariable)
-    def make_scatter(xvariable, yvariable, color_by):
-
-        color = update_colors(color_by)
+        self.data_df = pd.DataFrame(data[:self.index])[single_value_keys]
+        self.source = ColumnDataSource(data=self.data_df)
+        self.data = data
 
         plot_options=dict(plot_height=600, plot_width=900)
         plot_options['tools'] = [HoverTool(tooltips=[(k, "@{"+k+"}" + ("{0.00}" if k in float_keys else "")) for k in single_value_keys]), "box_select,box_zoom,save,reset"]
 
+        self.plot_options = plot_options
+
+        # setup widgets
+        self.yvariable = pnw.Select(name='Y', value='GFLOP/s', options=single_value_keys)
+        self.xvariable = pnw.Select(name='X', value='index', options=['index']+single_value_keys)
+        self.colorvariable = pnw.Select(name='Color By', value='GFLOP/s', options=single_value_keys)
+
+        self.scatter = pn.bind(self.make_scatter, xvariable=self.xvariable, yvariable=self.yvariable, color_by=self.colorvariable)
+
+        self.dashboard = pn.template.BootstrapTemplate(title='Kernel Tuner Dashboard')
+        self.dashboard.sidebar.append(pn.Column(self.yvariable, self.xvariable, self.colorvariable))
+        self.dashboard.main.append(self.scatter)
+        self.dashboard.servable()
+
+    def update_colors(self, color_by):
+        color_mapper = LinearColorMapper(palette='Viridis256', low=min(self.data_df[color_by]),
+                                         high=max(self.data_df[color_by]))
+        color = {'field': color_by, 'transform': color_mapper}
+        return color
+
+    def make_scatter(self, xvariable, yvariable, color_by):
+
+        color = self.update_colors(color_by)
+
         x = xvariable
         y = yvariable
 
-        f = figure(**plot_options)
-        f.circle(x, y, size=5, color=color, alpha=0.5, source=source)
+        f = figure(**self.plot_options)
+        f.circle(x, y, size=5, color=color, alpha=0.5, source=self.source)
         f.xaxis.axis_label = x
         f.yaxis.axis_label = y
 
-        pane = pn.pane.Bokeh(f)
+        pane = pn.Column(pn.pane.Markdown(f"## Auto-tuning {self.kernel_name} on {self.device_name}"), pn.pane.Bokeh(f))
+
         return pane
 
-    plot = make_scatter
+    def update_plot(self, i):
+        stream_dict = {k:[v] for k,v in dict(self.data[i], index=i).items() if k in ['index']+self.single_value_keys}
+        self.source.stream(stream_dict)
 
-    dashboard = pn.template.VanillaTemplate(title='Kernel Tuner Dashboard')
-    dashboard.sidebar.append(yvariable)
-    dashboard.sidebar.append(xvariable)
-    dashboard.sidebar.append(colorvariable)
-
-    dashboard.main.append(plot)
-
-    dashboard.servable();
-
-
-
-    def update_plot(i):
-        nonlocal data, source
-
-        print(f"update_plot called i={i}")
-
-        stream_dict = {k:[v] for k,v in dict(data[i], index=i).items() if k in ['index']+single_value_keys}
-        source.stream(stream_dict)
-
-
-    def update_data():
-
-        print("update_data called")
-        nonlocal data, index, source, cache_file
-
-        if os.stat(cache_file).st_mtime > cache_last_modified:
-            cached_data = read_cache(cache_file, open_cache=False)
+    def update_data(self):
+        if os.stat(self.cache_file).st_mtime > self.cache_last_modified:
+            cached_data = read_cache(self.cache_file, open_cache=False)
 
             data = list(cached_data["cache"].values())
             data = [d for d in data if d["time"] != 1e20]
 
-            #stream the data that has not been streamed yet
-            source.stream(pd.DataFrame([data[index:]])) #this may not work, we might need to convert it into a dict like in update_plot
-            index = len(data)
-
-        if demo:
-
-            if index < len(data)-1:
-                update_plot(index)
-                index += 1
-
-
-    def dashboard_f():
-        pn.state.add_periodic_callback(update_data, 1000)
-        return dashboard
-
-    server = pn.serve(dashboard_f)
-
+            # stream the data that has not been streamed yet
+            self.source.stream(pd.DataFrame([data[self.index:]])) # this may not work, we might need to convert it into a dict like in update_plot
+            self.index = len(self.data)
+        if self.demo:
+            if self.index < (len(self.data)-1):
+                self.update_plot(self.index)
+                self.index += 1
 
 
 
@@ -140,8 +112,6 @@ def print_usage():
     print("   -demo      option to enable demo mode that mimicks a running Kernel Tuner session")
     print("   filename   name of the cachefile")
     exit(0)
-
-
 
 
 if __name__ == "__main__":
@@ -165,5 +135,11 @@ if __name__ == "__main__":
         if os.path.isfile(sys.argv[2]):
             filename = sys.argv[2]
 
-    create_dashboard(filename, demo)
 
+    db = KTdashboard(filename, demo=demo)
+
+    def dashboard_f():
+        pn.state.add_periodic_callback(db.update_data, 1000)
+        return db.dashboard
+
+    server = pn.serve(dashboard_f)
